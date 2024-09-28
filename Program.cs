de0b0459a2ef799d132200504e8645663d40c9b1
Samara Justino <samarasouzajustino@gmail.com>
2024-09-28 17:42:11 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.Write("Qual o tamanho do arquivo? ");
+int tamArq = int.Parse(Console.ReadLine());
+Console.Write("Qual a velocidade da conexão? ");
+double velocidade = double.Parse(Console.ReadLine());
+double tempo = tamArq / velocidade;
+Console.WriteLine("O tempo de download é de " + tempo.ToString("F2") + " segundos" );
